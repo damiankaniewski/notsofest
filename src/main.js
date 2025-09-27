69788897efb8035dfd8bb1bff89dd5d4f554274c
Damian <daba9@op.pl>
2025-09-27 16:28:56 +0200

    vite config fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles.scss";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    assetsInclude: ["**/*.woff", "**/*.woff2", "**/*.ttf", "**/*.eot"],
+  },
 });

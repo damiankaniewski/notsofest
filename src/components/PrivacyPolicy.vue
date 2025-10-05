948b80ca6df2c90fe0590ce06793df1e8793da02
DrDaroo <cegladaroo@gmail.com>
2025-10-05 17:12:00 +0200

    PrivacyPolicy

--- src/components/PrivacyPolicy.vue
+++ src/components/PrivacyPolicy.vue
@@ -1,10 +1,357 @@
 <template>
   <SubHeader></SubHeader>
-  <section></section>
+  <section id="privacy-policy">
+    <div class="main-view">
+      <h1>Polityka prywatności</h1>
+      <div id="rules1">
+        <h2>
+          §1. Informacje ogólne
+        </h2>
+        <ol>
+          <li>
+            Niniejsza Polityka prywatności określa zasady przetwarzania i ochrony danych osobowych użytkowników strony internetowej Not So Fest, dostępnej pod adresem: <a href="/">www.notsofest.pl</a>.
+          </li>
+          <li>
+            Administratorem danych osobowych jest Jakub Troc.
+          </li>
+        </ol>
+      </div>
+      <div id="rules2">
+        <h2>
+          §2. Zakres zbieranych danych
+        </h2>
+          <div class="margin-div">
+            <p>
+              Administrator może przetwarzać następujące dane użytkowników:
+            </p>
+            <ul>
+              <li>imię i nazwisko,</li>
+              <li>adres e-mail,</li>
+              <li>numer IP,</li>
+              <li>informacje techniczne i statystyczne,</li>
+              <li>treść wiadomości przesyłanych przez formularz kontaktowy,</li>
+              <li>dane związane z zapisem do newslettera.</li>
+            </ul>
+          </div>
+      </div>
+      <div id="rules3">
+        <h2>
+          §3. Cele przetwarzania danych
+        </h2>
+        <div class="margin-div">
+          <p>
+            Dane osobowe są przetwarzane w następujących celach:
+          </p>
+          <ol>
+            <li>
+              Udzielenie odpowiedzi na zapytania przesłane przez formularz kontaktowy (na podstawie art. 6 ust. 1 lit. f RODO – prawnie uzasadniony interes).
+            </li>
+            <li>
+              Przesyłanie newslettera i informacji o wydarzeniach, jeśli użytkownik wyraził zgodę (na podstawie art. 6 ust. 1 lit. a RODO – zgoda).
+            </li>
+            <li>
+              Obsługa statystyk i analityki strony (na podstawie uzasadnionego interesu administratora).
+            </li>
+            <li>
+              Zabezpieczenie przed nadużyciami i spamem.
+            </li>
+          </ol>
+        </div>
+      </div>
+      <div id="rules4">
+        <h2>
+          §4. Newsletter
+        </h2>
+        <ol>
+          <li>
+            Zapis do newslettera jest dobrowolny i wymaga wyraźnej zgody.
+          </li>
+          <li>
+            Użytkownik może w każdej chwili wypisać się z newslettera, klikając w link rezygnacji znajdujący się w każdej wiadomości e-mail.
+          </li>
+          <li>
+            Dane są przechowywane do momentu wycofania zgody lub zakończenia działalności newslettera.
+          </li>
+        </ol>
+      </div>
+      <div id="rules5">
+        <h2>
+          §5. Formularz kontaktowy
+        </h2>
+        <ol>
+          <li>
+            Dane przesłane za pomocą formularza kontaktowego są przetwarzane wyłącznie w celu udzielenia odpowiedzi.
+          </li>
+          <li>
+            Wiadomości mogą być archiwizowane przez okres nie dłuższy niż 12 miesięcy od daty ostatniego kontaktu.
+          </li>
+        </ol>
+      </div>
+      <div id="rules6">
+        <h2>
+          §6. Pliki cookies i narzędzia śledzące
+        </h2>
+        <ol>
+          <li>
+            <p>
+              Strona wykorzystuje pliki cookies w celu:
+            </p>
+            <ul>
+              <li>
+                zapewnienia prawidłowego działania serwisu,
+              </li>
+              <li>
+                zbierania anonimowych statystyk,
+              </li>
+              <li>
+                funkcjonowania integracji z platformami zewnętrznymi.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Użytkownik może samodzielnie zarządzać plikami cookies w ustawieniach swojej przeglądarki internetowej.
+          </li>
+          <li>
+            Korzystanie ze strony bez zmiany ustawień dotyczących cookies oznacza zgodę na ich użycie.
+          </li>
+        </ol>
+      </div>
+      <div id="rules7">
+        <h2>
+          §7. Udostępnianie danych
+        </h2>
+        <ol>
+          <li>
+            <p>
+              Dane użytkowników nie są przekazywane podmiotom trzecim bez wyraźnej zgody, z wyjątkiem:
+            </p>
+            <ul>
+              <li>
+                podmiotów obsługujących infrastrukturę techniczną strony,
+              </li>
+              <li>
+                dostawców narzędzi statystycznych,
+              </li>
+              <li>
+                sytuacji wymaganych przez przepisy prawa.
+              </li>
+            </ul>
+          </li>
+        </ol>
+      </div>
+      <div id="rules8">
+        <h2>
+          §8. Prawa użytkownika
+        </h2>
+          <div class="margin-div">
+            <p>
+              Każda osoba, której dane są przetwarzane, ma prawo do:
+            </p>
+            <ul>
+              <li>
+                dostępu do swoich danych,
+              </li>
+              <li>
+                sprostowania danych,
+              </li>
+              <li>
+                usunięcia danych („prawo do bycia zapomnianym”),
+              </li>
+              <li>
+                ograniczenia przetwarzania,
+              </li>
+              <li>
+                przenoszenia danych,
+              </li>
+              <li>
+                wniesienia sprzeciwu wobec przetwarzania,
+              </li>
+              <li>
+                wycofania zgody w dowolnym momencie (bez wpływu na legalność przetwarzania przed cofnięciem zgody),
+              </li>
+              <li>
+                wniesienia skargi do Prezesa Urzędu Ochrony Danych Osobowych (UODO).
+              </li>
+            </ul>
+          </div>
+      </div>
+      <div id="rules9">
+        <h2>
+          §9. Okres przechowywania danych
+        </h2>
+        <div class="margin-div">
+          <p>
+            Dane będą przechowywane przez okres niezbędny do realizacji celów przetwarzania, zgodnie z zasadami minimalizacji danych:
+          </p>
+          <ul>
+            <li>
+              formularz kontaktowy – do 12 miesięcy,
+            </li>
+            <li>
+              newsletter – do czasu wycofania zgody,
+            </li>
+            <li>
+              statystyki – zgodnie z ustawieniami narzędzi analitycznych.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="rules10">
+        <h2>
+          §10. Zmiany polityki prywatności
+        </h2>
+        <div class="margin-div">
+          <p>
+            Administrator zastrzega sobie prawo do wprowadzania zmian w niniejszej Polityce prywatności, o czym użytkownicy zostaną poinformowani poprzez aktualizację treści na stronie.
+          </p>
+        </div>
+      </div>
+      <div id="rules11">
+        <h2>
+          §11. Kontakt
+        </h2>
+        <div class="margin-div">
+          <p>
+            W sprawach związanych z przetwarzaniem danych osobowych można kontaktować się z Administratorem poprzez adres email” kontakt@notsofest.pl
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
   import SubHeader from "./utils/SubdomainHeader.vue";
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+  .main-view {
+    margin-top: 140px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    min-height: 100svh;
+
+    div {
+      width: 78%;
+      padding: 0 4rem;
+      padding-right: 7rem;
+      text-align: center;
+      justify-content: center;
+
+      ol {
+        margin-left: 1rem;
+        text-align: start;
+        display: flex;
+        flex-direction: column;
+        gap: 1.4rem;
+      }
+    }
+  }
+
+  ul{
+    list-style-type: disc;
+    margin-left: 1rem;
+    text-align: start;
+    display: flex;
+    flex-direction: column;
+    gap: 1.4rem;
+  }
+
+  .margin-div {
+    gap: 0;
+    width: 100% !important;
+    margin-left: 2rem;
+    padding: 0 !important; 
+  }
+  
+  p {
+    margin-top: 0;
+    text-align: start;
+    color: white;
+    font-size: 1.4rem;
+  }
+
+  h1 {
+    color: #FF6600;
+    font-size: 6rem;
+    margin: 0;
+  }
+
+  h2 {
+    text-align: start;
+    color: #FF6600;
+    font-size: 1.8rem;
+  }
+
+  li {
+    color: white;
+    font-size: 1.4rem;
+  }
+
+  @media (max-width: 1280px){
+    .main-view{
+      div {
+        width: 70%;
+        padding-right: 10rem;
+      }
+    }
+
+    h1 {
+      font-size: 4.8rem;
+    }
+
+    h2{
+      font-size: 1.6rem;  
+    }
+    li,p {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (max-width: 1024px){
+    .main-view {
+      div {
+        width: 60%;
+      }
+    }
+    h1 {
+      font-size: 3.6rem;
+    }
+    h2 {
+      font-size: 1.4rem;
+    }
+    li,p {
+      font-size: 1rem;
+    }
+  }
+
+  @media (max-width: 768px){
+    .main-view {
+      div {
+        width: 80%;
+        padding: 0 2rem;
+        ol {
+          margin-left: 0;
+        }
+        ul {
+          margin-left: 0;
+        }
+      }
+    }
+
+    .margin-div {
+      margin-left: 1rem;
+    }
+    h1 {
+      font-size: 2rem;
+    }
+    h2 {
+      font-size: 1rem;
+    }
+    li, p {
+      font-size: 0.8rem;
+    }
+  }
+</style>
